ad2d3d35425f34e18335c9abab5ab88df40dfa64
ManuelP720 <m.perezp2@uniandes.edu.co>
2021-08-22 19:06:26 -0500

    Cambios del estudiante 2

--- App/view.py
+++ App/view.py
@@ -87,7 +87,7 @@ while True:
 
         # TODO: Modificación de Est-2 en el Lab 2
 
-    elif int(inpputs[0]) == 2:
+    elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
